8623a344c58117a3d498109c45ff784f7933c087
Creighton Higgins <chiggins@chiggins.com>
2014-08-06 13:56:09 -0700

    add dynamic title and tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,12 +9,25 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    # title!
+    it "should have title 'RoRT Sample App | Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('RoRT Sample App | Home')
+    end
+
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
       visit "/static_pages/help"
       expect(page).to have_content('Help')
+    end
+
+    # title!
+    it "should have title 'RoRT Sample App | Title'" do
+      visit '/static_pages/help'
+      expect(page).to have_title('RoRT Sample App | Help')
     end
   end
 
@@ -23,6 +36,13 @@ describe "StaticPages" do
       visit "/static_pages/about"
       expect(page).to have_content('About')
     end
+
+    # title!
+    it "should have title 'RoRT Sample App | About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('RoRT Sample App | About')
+    end
+
   end
 
 end
