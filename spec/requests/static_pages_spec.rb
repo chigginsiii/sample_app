8d8f2d7173033bc9d6b947a302f8f4b4002b5b07
Creighton Higgins <chiggins@chiggins.com>
2014-08-08 14:37:40 -0700

    clean up static pages test with cleaner RSpec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,64 +4,30 @@ describe "StaticPages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
-
-
+  subject { page }
   describe "Home page" do
-    # this said to run it again if I want to use webrat functions,
-    # i'm assuming webrat is an alternative to capybara?
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-
-    # title!
-    it "should have title base_title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title}")
-    end
-    it "should not have a custom title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title("| Home")
-    end
-
+    before {  visit '/' }
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title(full_title("Home")) }
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit "/static_pages/help"
-      expect(page).to have_content('Help')
-    end
-
-    # title!
-    it "should have title 'RoRT Sample App | Title'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    before {  visit '/help' }
+    it { should have_content('Help') }
+    it { should have_title(full_title("Help")) }
   end
 
   describe "About page" do
-    it "should have the content 'About'" do
-      visit "/static_pages/about"
-      expect(page).to have_content('About')
-    end
-
-    # title!
-    it "should have title 'RoRT Sample App | About'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About")
-    end
-
+    before {  visit "/about" }
+    it { should have_content('About') }
+    it { should have_title(full_title("About")) }
   end
 
   describe "Contact page" do
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('Contact')
-    end
-    it "should have the title 'RoRT Sample App | Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} | Contact")
-    end
+    before {  visit "/contact" }
+    it { should have_content('Contact') }
+    it { should have_title(full_title("Contact")) }
   end
 
 end
