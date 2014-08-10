8374fda34fc787b225a3aeb771c9143002d90e1b
Creighton Higgins <chiggins@chiggins.com>
2014-08-10 15:32:44 -0700

    update users success and fail

--- spec/requests/users_pages_spec.rb
+++ spec/requests/users_pages_spec.rb
@@ -30,11 +30,8 @@ describe "UsersPages" do
 
       describe "after submission" do
         before do
-          fill_in "Name", with: "Example User"
-          fill_in "Email", with: "user@example"
-          fill_in "Password", with: "fooba"
-          fill_in "Confirmation", with: "toobar"
-          click_button submit
+          fill_in_invalid_signup
+          click_button 'Create my account'
         end
         it { should have_title("Sign Up") }
         it { should have_content("3 errors") }
@@ -46,11 +43,9 @@ describe "UsersPages" do
 
     describe "with valid information" do
       before do
-        fill_in "Name", with: "Example User"
-        fill_in "Email", with: "user@example.com"
-        fill_in "Password", with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in_valid_signup
       end
+
       it "should create a user" do
         expect{ click_button submit }.to change(User, :count).by(1)
       end
@@ -62,8 +57,55 @@ describe "UsersPages" do
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
         it { should have_link('Sign Out') }
       end
-
     end
   end
 
-end
+  describe "edit" do
+
+    describe "page" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        sign_in user
+        visit edit_user_path(user)
+      end
+
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit Profile") }
+      it { should have_link('Change', href: "http://gravatar.com/email") }
+
+      # XXX: this describes the menu of any logged in page, should RSpec::Matcher it
+      it { should have_link("Account") }
+      describe "after clicking Account" do
+        before { click_link "Account" }
+        it { should have_link("Profile", href: user_path(user) ) }
+        it { should have_link("Settings", href: edit_user_path(user)) }
+        it { should have_link("Sign Out", href: signout_path) }
+      end
+    end
+
+    describe "update" do
+
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        sign_in user
+        visit edit_user_path(user)
+      end
+
+      describe "with invalid information" do
+        before { click_button "Save Changes" }
+        it { should have_error }
+      end
+
+      describe "with valid information" do
+        before do
+          fill_in_valid_signup
+          fill_in "Name", with: "CDH"
+          click_button 'Save Changes'
+        end
+        it { should have_success("Profile updated!") }
+      end
+    end #update
+
+  end #edit
+
+end #UsersPages
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -11,6 +11,34 @@ def click_signout
   click_link "Sign Out"
 end
 
+def fill_in_valid_signup
+  fill_in "Name", with: "Example User"
+  fill_in "Email", with: "user@example.com"
+  fill_in "Password", with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
+
+def fill_in_invalid_signup
+  fill_in "Name", with: "Example User"
+  fill_in "Email", with: "user@example"
+  fill_in "Password", with: "fooba"
+  fill_in "Confirmation", with: "toobar"
+end
+
+# this one's to give us sign-in capability without capybara
+def sign_in( user, options: {} )
+  if options[:no_capybara]
+    remember_token = User.new_remember_token
+    cookies[:rememer_token] = remember_token
+    user.update_attribute(User.digest(remember_token))
+  else
+    visit signin_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign In"
+  end
+end
+
 RSpec::Matchers.define :be_signed_out do
   match do |page|
     expect(page).to have_link("Sign In")
@@ -25,6 +53,25 @@ RSpec::Matchers.define :be_signed_in do |user|
     expect(page).to have_title(user.name)
     expect(page).not_to have_link('Sign In', href: signin_path)
     expect(page).to have_link('Profile',  href: user_path(user))
+    expect(page).to have_link('Settings', href: edit_user_path(user))
     expect(page).to have_link('Sign Out', href: signout_path)
+  end
+end
+
+RSpec::Matchers.define :have_error do |msg|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: msg)
+  end
+end
+
+RSpec::Matchers.define :not_have_error do
+  match do |page|
+    expect(page).not_to have_selector('div.alert.alert-error')
+  end
+end
+
+RSpec::Matchers.define :have_success do |msg|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-success', text: msg)
   end
 end
