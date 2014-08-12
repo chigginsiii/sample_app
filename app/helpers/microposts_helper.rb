1f339946d4aecb4a25e3fbe66c549dae66eee4d1
Creighton Higgins <chiggins@chiggins.com>
2014-08-12 10:50:36 -0700

    wrap long words

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -7,4 +7,14 @@ module MicropostsHelper
             title: micropost.content )
   end
 
+  def wrap
+    sanitize( raw( content.split.map{ |s| wrap_long_string(s) }.join(' ') ))
+  end
+
+  def wrap_long_string(text, max_width = 30)
+    zero_width_space = "&#8203;"
+    regex = /.{1,#{max_width}}/
+    ( text.length < max_width ) ? text :
+                                  text.scan(regex).join(zero_width_space)
+  end
 end
